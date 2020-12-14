cc6367686ba20b2b41a55cc2538ef11fd0a2552b
36t <saluta@gmail.com>
2020-12-14 16:12:30 +0900

    fix: fixed eslint error (return type).

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg'
 import { Counter } from './features/counter/Counter'
 import './App.css'
 
-function App () {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
